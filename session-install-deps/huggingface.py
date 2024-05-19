6bd373382cd617b6951060f31dd9526c132beebb
itay segev <22itay@users.noreply.github.com>
2024-05-19 18:17:16 +0300

    add Download Huggingface Models

---
+++ session-install-deps/huggingface.py
@@ -0,0 +1,6 @@
+!huggingface-cli download imvladikon/wav2vec2-xls-r-300m-hebrew
+!huggingface-cli download yam-peleg/Hebrew-Gemma-11B-Instruct
+!huggingface-cli download yam-peleg/Hebrew-Mixtral-8x22B
+!huggingface-cli download yam-peleg/Hebrew-Mistral-7B
+!huggingface-cli download yam-peleg/Hebrew-Gemma-11B-V2
+!huggingface-cli download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2
